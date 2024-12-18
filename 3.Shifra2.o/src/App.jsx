e1a7b01beec01a42656519f848bdbf5b60834f78
michelle23-code <michellelekoto13@icloud.com>
2024-12-19 01:09:44 +0200

    fixed a bug

--- 3.Shifra2.o/src/App.jsx
+++ 3.Shifra2.o/src/App.jsx
@@ -1,24 +1,33 @@
 import { CiMicrophoneOn } from "react-icons/ci";
+import "./App.css"
+import va from "./assets/ai.png"
+import React,{useContext} from "react";
 import { datacontext } from "./context/UserContext";
 import speakimg from "./assets/speak.gif";
+import aigif from "./assets/aivoice.gif";
 
 function App() {
-  let { recognition,speaking,setSpeaking }=useContext(datacontext)
+  let { recognition,speaking,setSpeaking,prompt,response,setPrompt,setResponse }=useContext(datacontext)
 
   return (
     <div className="main">
       <img src={va} alt="" id="shifra" />
-      <span>I'm Shifra,Your Advanced virtual Assistant</span>
-      {!speaking ? <button
-          onClick={()=>{
+      <span>I'm Shifra,Your Advanced virtual Assistant     
+      </span>
+      {!speaking?
+       <button onClick={()=>{
+            setPrompt("listening...")
             setSpeaking(true)
+            setResponse(true)
             recognition.start()
-          }}>
-          Click here <CiMicrophoneOn /> </button>
-       : 
-        <div className="resoponse">
-          <img src={speakimg} alt="" id="speak" />
-          <p>listeing</p>
+          }}>Click here <CiMicrophoneOn /></button>
+           : 
+        <div className='resoponse'>
+          {!response?
+          <img src={speakimg} alt="" id="speak" /> 
+           :
+          <img src={aigif} alt="" id="aigif" />}
+          <p>{prompt}</p>
         </div>
 }
      </div>
@@ -27,4 +36,4 @@ function App() {
   )
 }
 
-export default App;
+export default App
--- 3.Shifra2.o/src/context/UserContext.jsx
+++ 3.Shifra2.o/src/context/UserContext.jsx
@@ -1,36 +1,61 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState } from 'react'
 export const datacontext = createContext()
 import run from "../gemini";
 
 function UserContext({children}) {
   let [speaking, setSpeaking]=useState(false)
   let [prompt,setPrompt]=useState("listening...")
+  let [resoponse,setResponse]=useState(false)
 
   function speak(text){
-    let text_speak = new SpeechSynthesisUtterance(text)
-    text_speak.volume = 1;
-    text_speak.rate = 1;
-    text_speak.pitch = 1;
-    text_speak.lang = "en-GB";
+    let text_speak= new SpeechSynthesisUtterance(text)
+    text_speak.volume= 1;
+    text_speak.rate= 1;
+    text_speak.pitch= 1;
+    text_speak.lang= "en-GB";
     window.speechSynthesis.speak(text_speak)
   }
-  async function aiResponse(prompt) {
-    let text = await run(prompt);
-    speak(text);
+  async function aiResponse(prompt){
+    let text =await run(prompt)
+    let newText=text.split("**")&&text.split("**")&&text.replace("google","Pontsho Lekoto")&&text.replace("google","Pontsho Lekoto")
+    setPrompt(newText)
+    speak(newText)
+    setResponse(true)
+    setTimeout(()=>{
+      setSpeaking(false)
+    },6000)
+    
   }
   let speechRecognition =window.speechRecognition || window.webkitSpeechRecognition;
   let recognition = new speechRecognition()
   recognition.onresult = (e) => {
-    let currentIndex = e.resultIndex
-    let transcript = e.results[currentIndex][0].transcript
+    let currentIndex =e.resultIndex
+    let transcript =e.results[currentIndex][0].transcript
     setPrompt(transcript)
-    aiResponse(transcript)
+    takeCommand(transcript.toLowerCase())
+  }
+
+  function takeCommand(command){
+    if(command.includes("open")&& command.includes("youtube")){
+      window.open("https://www.youtube.com/","_blank")
+      speak("opening Youtube")
+      setPrompt("opening Youtube...")
+      setTimeout(()=>{
+        setSpeaking(false)
+      },6000)
+    }
+    else{
+      aiResponse(command)
+    }
   }
 
   let value = {
     recognition,
     speaking,
     setSpeaking,
+    prompt,
+    setPrompt,
+    setResponse
   }
   return (
     <div>
